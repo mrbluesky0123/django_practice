cb7da948ce00b9076b3a2c591780864c6a5b2f9c
mrbluesky <onightraino@gmail.com>
2019-01-30 08:24:19 +0900

    201901130

--- restful01/toys/serializers.py
+++ restful01/toys/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 from toys.models import Toy
-
+'''
 class ToySerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=150)
     description = serializers.CharField(max_length=250)
-    release_date = serializers.DateField()
+    release_date = serializers.DateTimeField()
     toy_category = serializers.CharField(max_length=200)
     was_included_in_home = serializers.BooleanField(required=False)
 
@@ -20,3 +20,10 @@ class ToySerializer(serializers.Serializer):
         instance.was_included_in_home = validated_data.get('was_included_in_home', instance.was_included_in_home)
         instance.save()
         return instance
+'''
+
+class ToySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Toy
+        fields = ('id', 'name', 'description', 'release_date', 'toy_category', 'was_included_in_home' )
+
--- restful01/toys/views.py
+++ restful01/toys/views.py
@@ -1,3 +1,94 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+# from django.http import HttpResponse 
+# from django.views.decorators.csrf import csrf_exempt 
+# from rest_framework.renderers import JSONRenderer 
+# from rest_framework.parsers import JSONParser 
+from rest_framework import status 
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
+from toys.models import Toy 
+from toys.serializers import ToySerializer 
 
-# Create your views here.
+'''
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def toy_list(request):
+    if request.method == 'GET':
+        toys = Toy.objects.all()
+        toy_serializer = ToySerializer(toys, many=True)
+        return JSONResponse(toy_serializer.data)
+    
+    elif request.method == 'POST':
+        toy_data = JSONParser().parse(request)
+        toy_serializer = ToySerializer(data=toy_data)
+        if toy_serializer.is_valid():
+            toy_serializer.save()
+            return JSONResponse(toy_serializer.data, status=status.HTTP_201_CREATED)
+        return JSONResponse(toy_serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+@csrf_exempt
+def toy_detail(request, pk):
+    try:
+        toy = Toy.objects.get(pk=pk)
+    except Toy.DoesNotExist:
+        return HttpResponse(status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        toy_serializer = ToySerializer(toy)
+        return JSONResponse(toy_serializer.data)
+    
+    elif request.method == 'PUT':
+        toy_data = JSONParser().parse(request)
+        toy_serializer = ToySerializer(toy, data = toy_data)
+        if toy_serializer.is_valid():
+            toy_serializer.save()
+            return JSONResponse(toy_serializer.data)
+        return JSONResponse(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        toy.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        '''
+@api_view(['GET', 'POST'])
+def toy_list(request):
+    if request.method == 'GET':
+        toys = Toy.objects.all()
+        toy_serializer = ToySerializer(toys, many=True)
+        return Response(toy_serializer.data)
+    
+    elif request.method == 'POST':
+        # toy_data = JSONParser().parse(request)
+        toy_serializer = ToySerializer(data=toy_data)
+        if toy_serializer.is_valid():
+            toy_serializer.save()
+            return Response(toy_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(toy_serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def toy_detail(request, pk):
+    try:
+        toy = Toy.objects.get(pk=pk)
+    except Toy.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        toy_serializer = ToySerializer(toy)
+        return Response(toy_serializer.data)
+    
+    elif request.method == 'PUT':
+        # toy_data = JSONParser().parse(request)
+        toy_serializer = ToySerializer(toy, data = toy_data)
+        if toy_serializer.is_valid():
+            toy_serializer.save()
+            return Response(toy_serializer.data)
+        return Response(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        toy.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
